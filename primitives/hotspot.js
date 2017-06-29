b5a0fd867af0cc377c6c04c5846839ee733e4fc8
caseyyee <caseyyee.ca@gmail.com>
2017-06-29 12:48:38 -0700

    cleanup events code

--- primitives/hotspot.js
+++ primitives/hotspot.js
@@ -37,57 +37,45 @@ AFRAME.registerPrimitive('a-hotspot-helper', {
 
 AFRAME.registerComponent('hotspot-helper', {
   schema: {
-    distance: { type: 'number', default: 5 }
+    distance: { type: 'number', default: 5 },
+    distanceIncrement: { type: 'number', default: 0.25 }
   },
 
   init: function () {
+    var self = this;
     this.camera = document.querySelector('a-entity[camera]');
 
-    var panelEl = document.createElement('div');
-    panelEl.style.cssText = 'position: absolute; top: 0; left: 0; padding: 10px; background: black; color: white; font-family: Helvetica, Arial, Sans-Serif';
-    document.body.appendChild(panelEl);
+    var uiContainer = document.createElement('div');
+    uiContainer.style.cssText = 'position: absolute; top: 0; left: 0; padding: 10px; background: black; color: white; font-family: Helvetica, Arial, Sans-Serif';
+    document.body.appendChild(uiContainer);
 
-    var distanceEl = document.createElement('input');
-    distanceEl.type = "text";
-    distanceEl.size = 4;
-    distanceEl.value = this.data.distance;
-    var self = this;
-    distanceEl.addEventListener('input', function () {
+    var distanceInput = document.createElement('input');
+    distanceInput.type = "text";
+    distanceInput.size = 4;
+    distanceInput.value = this.data.distance;
+    this.distanceInput = distanceInput;
+    distanceInput.addEventListener('input', function () {
       self.updateDistance(this.value);
     });
+    uiContainer.appendChild(distanceInput);
 
-    distanceEl.addEventListener('mousewheel', function (e) {
-      var value = e.deltaY < 0 ? 1 : -1;
-      this.value = parseInt(this.value) + value;
-      self.updateDistance(this.value);
-    });
+    // mousewheel distance
+    document.body.addEventListener('mousewheel', this.handleMouseWheel.bind(this));
 
-    panelEl.appendChild(distanceEl);
-
+    // coordinates
     var outEl = document.createElement('div');
-    panelEl.appendChild(outEl);
+    uiContainer.appendChild(outEl);
     this.out = outEl;
 
+    // copy coordinates into clipboard
     var copyEl = document.createElement('button');
     copyEl.innerHTML = 'copy';
-    copyEl.addEventListener('click', function () {
-      var selection = window.getSelection();    // Save the selection.
-      var range = document.createRange();
-      range.selectNodeContents(outEl);
-      selection.removeAllRanges();          // Remove all ranges from the selection.
-      selection.addRange(range);
+    uiContainer.appendChild(copyEl);
+    copyEl.addEventListener('click', this.copyCordinates.bind(this));
 
-      try {
-        // Now that we've selected the anchor text, execute the copy command
-        var successful = document.execCommand('copy');
-      } catch(err) {
-        console.log('Oops, unable to copy');
-      }
-    })
-    panelEl.appendChild(copyEl);
-
-    var geometry = new THREE.BoxGeometry( 0.1, 0.1, 0.1 );
-    var material = new THREE.MeshBasicMaterial( {color: 0x00ff00} );
+    // reference mesh for position.
+    var geometry = new THREE.BoxGeometry( 0.1, 0.2, 0.1 );
+    var material = new THREE.MeshBasicMaterial({ color: 0x00ff9c });
     var cube = new THREE.Mesh( geometry, material );
     this.cube = cube;
     this.el.object3D.add( cube );
@@ -98,22 +86,32 @@ AFRAME.registerComponent('hotspot-helper', {
     this.cube.position.z = -distance;
   },
 
-  tick: function () {
-    // use camera world rotation.
-    var rotation = this.camera.object3D.getWorldRotation();
-
-    this.el.object3D.rotation.copy(rotation);
-    var position = this.cube.getWorldPosition();
-
-    //this.el.object3D.rotation = rotation;
-    //var quarternion = this.el.object3D.quaternion;
-    var cords = this.roundTo(position.x, 3) + ' ' + this.roundTo(position.y, 3) + ' ' + this.roundTo(position.z, 3);
-    this.out.innerHTML = cords;
-    //this.out.setAttribute('text', { value: cords });
-    //var rotation = this.camera.getAttribute('rotation');
+  roundTo: function(num, x) {
+    return +(Math.round(num + 'e+' + x)  + 'e-' + x);
   },
 
-  roundTo: function(num, x) {
-    return +(Math.round(num + "e+" + x)  + "e-" + x);
+  copyCordinates: function () {
+    var selection = window.getSelection();
+    var range = document.createRange();
+    range.selectNodeContents(this.out);
+    selection.removeAllRanges();
+    selection.addRange(range);
+    document.execCommand('copy');
+  },
+
+  handleMouseWheel: function (e) {
+    var input = this.distanceInput;
+    var data = this.data;
+    var increment = e.deltaY < 0 ? data.distanceIncrement : -data.distanceIncrement;
+    input.value = parseFloat(input.value) + increment;
+    this.updateDistance(input.value);
+  },
+
+  tick: function () {
+    var rotation = this.camera.object3D.getWorldRotation();
+    this.el.object3D.rotation.copy(rotation);
+    var position = this.cube.getWorldPosition();
+    var cords = this.roundTo(position.x, 3) + ' ' + this.roundTo(position.y, 3) + ' ' + this.roundTo(position.z, 3);
+    this.out.innerHTML = cords;
   }
 });
